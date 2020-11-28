c12e31f4425ffa3950250051adb057cdc4df7782
vitaTiZ <tizianovitaliani@gmail.com>
2020-11-28 14:18:07 +0100

    add test for quotes api response and FE data

--- tests/cypress/integration/common/keyPressApp.js
+++ tests/cypress/integration/common/keyPressApp.js
@@ -6,30 +6,38 @@ Given('I access to app', function () {
         "method": "GET"
     }).as('quote')
     cy.visit('/')
-    cy.wait('@quote')
-    // .then(xhr=>{
-    //     cy.log(xhr)
-    //     var resp = JSON.parse(JSON.stringify(xhr.body))
-    //     cy.log(resp)
-    // })
-})
+    cy.wait('@quote').then(xhr => {
+        cy.log(xhr)
+        var resp = JSON.parse(JSON.stringify(xhr.response))
+        expect(resp.statusCode).to.be.equal(200)
+        var quotes = JSON.parse(resp.body)
+        this.map.set('quotes', quotes)
+    })
 
-When('I see a text', function () {
     cy.get('body home-page')
         .shadow()
         .find('game-view')
         .shadow()
         .find('div[class="content-container"] > wc-text-highlightable')
         .shadow()
-        .find('[data-qa="quote-text"]')
-        .should('exist')
-        .should('be.visible').as('text')
+        .find('[data-qa="quote-text"]').as('text')
+        .invoke('text').then(text => {
+            this.map.set('writedText', text.trim())
+        })
 
-    cy.get('@text').invoke('text').then(text => {
-        cy.log(text)
-        this.map.set('writedText', text.trim())
+    cy.get('body home-page')
+        .shadow()
+        .find('game-view')
+        .shadow()
+        .find('div[class="content-container"]')
+        .find('[data-qa="quote-author"]').as('quoteAuthor')
+        .invoke('text').then(quoteAuthor => {
+            this.map.set('quoteAuthor', quoteAuthor.trim())
+        })
+})
 
-    })
+When('I see a text', function () {
+    cy.get('@text').should('exist').should('be.visible')
 })
 
 Then('I write the text', function () {
@@ -53,7 +61,6 @@ Then('I write the text', function () {
         .find('[data-qa="quote-text"')
         .find('[data-qa="quote-highLighted-text"]').invoke('text').then(highLightedText => {
             this.map.set('highLightedText', highLightedText)
-            // expect(highLightedText.trim()).to.be.equal(writedText.trim())
         })
 })
 
@@ -71,11 +78,25 @@ Then('I see a navbar', function () {
     cy.get('@navbar').find('[data-qa="donate"]').should('be.visible')
 })
 
+Then('the system return correct quote and author', function () {
+    const quoteText = this.map.get('writedText')
+    const quoteAuthor = this.map.get('quoteAuthor')
+    const quotes = this.map.get('quotes')
+    var quoteFound = false
+    quotes.forEach(quote=>{
+        if(quote.text == quoteText && quote.author == quoteAuthor){
+            quoteFound = true
+        }
+    })
+    expect(quoteFound).to.be.true
+})
+
 And('the system highlights the text writed', function () {
     var writedText = this.map.get('writedText')
     var highLightedText = this.map.get('highLightedText')
-    
+
     expect(highLightedText.trim()).to.be.equal(writedText.trim())
+    cy.log(this.map)
 })
 
 
--- tests/cypress/integration/features/keyPressApp.feature
+++ tests/cypress/integration/features/keyPressApp.feature
@@ -2,13 +2,17 @@ Feature: KeypressApp
 
     Test e2e for keyPressApp
 
-    @focus
+
     Scenario: write text test
         Given I access to app
         When I see a text
         Then I write the text
             And the system highlights the text writed
 
+    @focus
+    Scenario: write text test
+        Given I access to app
+        Then the system return correct quote and author
 
     Scenario: navbar
         Given I access to app
